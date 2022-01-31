e12a6c150e0c8f502f61932b2f632e5312cc1f00
hgutierrezg <heidyguti@gmail.com>
2022-01-31 14:08:20 -0500

    Adding list controller

---
+++ src/main/webapp/components/timesheet-list/timesheet-list.component.js
@@ -0,0 +1,58 @@
+/**
+ * Creating controller only inside sharedTimesheetApp module,
+ * so the controller does not become a global function
+ */
+'use strict';
+angular.module('sharedTimesheetApp')
+    .component(
+        'timesheetlist',
+        {
+            controller: TimesheetListController,
+            templateUrl: 'components/timesheet-list/timesheet-list.html',
+            controllerAs: 'timesheetListController',
+            bindings: {
+                role: '<',
+            }
+        });
+
+function TimesheetListController (timesheetService, $scope) {
+
+        var timesheetController = this;
+        timesheetController.timesheets = [];
+        timesheetController.approve = approve;
+
+        getAllTimesheets();
+        function getAllTimesheets() {
+            timesheetService.getAllTimesheets()
+                .then(
+                    function (response) {
+                        timesheetController.timesheets = response;
+                    },
+                    function () {
+                        console.error('Error while reading timesheets with error ' + errResponse);
+                    }
+                );
+        }
+
+        function updateTimesheet(timesheet, id) {
+            timesheetService.updateTimesheet(timesheet, id)
+                .then(
+                    getAllTimesheets,
+                    function (errResponse) {
+                        console.error('Error while updating timesheet with error ' + errResponse);
+                    }
+                );
+        }
+
+        function approve(timesheet) {
+            timesheet.approved = true;
+            updateTimesheet(timesheet);
+        }
+
+    /**
+     * scope listening to event emitted with the name of 'timesheetCreated'
+      */
+    $scope.$on("timesheetCreated", function (evt) {
+        getAllTimesheets();
+    });
+};
