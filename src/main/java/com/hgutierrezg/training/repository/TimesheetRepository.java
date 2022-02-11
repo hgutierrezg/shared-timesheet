7276bfeabf81eaaaa02dc2c215cd6497c2ef805c
hgutierrezg <heidyguti@gmail.com>
2022-02-11 10:25:26 -0500

    Removing class extension from repository

--- src/main/java/com/hgutierrezg/training/repository/TimesheetRepository.java
+++ src/main/java/com/hgutierrezg/training/repository/TimesheetRepository.java
@@ -2,9 +2,8 @@ package com.hgutierrezg.training.repository;
 
 import com.hgutierrezg.training.model.TimesheetEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface TimesheetRepository extends
-        JpaRepository <TimesheetEntity, Long>, JpaSpecificationExecutor <TimesheetEntity> {
+        JpaRepository <TimesheetEntity, Long> {
 
 }
